ea90167c80165a6035f2995ed46d5aa394a37147
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-07-28 23:35:58 +0500

    feature: add Logout button component and integrate it into the Nav component; update environment variable references in Keys.js

---
+++ src/components/Button/Logout.jsx
@@ -0,0 +1,18 @@
+import { useDispatch } from "react-redux";
+import appAuth from "../../app/AuthService";
+import { logout } from "../../store/reducers/authSlice";
+const Logout = () => {
+      const dispatch = useDispatch();
+      const handleLogout = () => {
+            appAuth.Logout().then(() => {
+                  dispatch(logout());
+            });
+      };
+      return (
+            <button onClick={handleLogout} className="px-3 py-2 text-sm tracking-tight leading-none bg-red-200 text-red-500 rounded-full">
+                  Logout
+            </button>
+      );
+};
+
+export default Logout;
--- src/config/Keys.js
+++ src/config/Keys.js
@@ -1,7 +1,7 @@
 export default {
-      base_url: String(import.meta.env.APPWRITE_BASE_URL),
-      project_id: String(import.meta.env.APPWRITE_PROJECT_ID),
-      db_id: String(import.meta.env.APPWRITE_DATABASE_ID),
-      article_collection_id: String(import.meta.env.APPWRITE_ARTICLE_COLLECTION_ID),
-      image_bucket_id: String(import.meta.env.APPWRITE_IMAGES_BUCKET_ID),
+      base_url: String(import.meta.env.VITE_APPWRITE_BASE_URL),
+      project_id: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+      db_id: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+      article_collection_id: String(import.meta.env.VITE_APPWRITE_ARTICLE_COLLECTION_ID),
+      image_bucket_id: String(import.meta.env.VITE_APPWRITE_IMAGES_BUCKET_ID),
 };
