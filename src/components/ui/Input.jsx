eda6f17dda7e603a5ede66964bdd1a083d71c4b3
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-07-29 15:51:15 +0500

    feature: add Input component and update index file to export all components

---
+++ src/components/ui/Input.jsx
@@ -0,0 +1,17 @@
+import { useId } from "react";
+
+const Input = ({ label, type = "text", placeholder = "Enter something", star = false, className = "", ref, ...attributes }) => {
+      const id = useId();
+      return (
+            <div className="col-span-2 ">
+                  {label && (
+                        <label htmlFor={id}>
+                              {label} {star && <span className="text-red-500">*</span>}
+                        </label>
+                  )}
+                  <input ref={ref} id={id} className={`px-4 w-full py-2 block border-[0.5px] rounded-xl outline-none ${className}`} type={type} placeholder={placeholder} {...attributes} />
+            </div>
+      );
+};
+
+export default Input;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import Login from "./Login";
+import Posts from "./Posts";
+import Signup from "./Signup";
+import ViewPost from "./ViewPost";
+import WritePost from "./WritePost";
+export { Home, Login, Posts, Signup, ViewPost, WritePost };
