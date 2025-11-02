b07f9f8dcb8736e6040a35ec0dfba753c2c3b84d
MAhmad25 <userarenotproduct@gmail.com>
2025-11-02 15:06:23 +0500

    Remove GSAP animation logic and refs from ScreenLoader component for simplification

--- src/components/ui/ScreenLoader.jsx
+++ src/components/ui/ScreenLoader.jsx
@@ -1,19 +1,4 @@
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { useRef } from "react";
-
 const ScreenLoader = () => {
-      const logo = useRef(null);
-      const img = useRef(null);
-      useGSAP(() => {
-            const tl = gsap.timeline({ defaults: { duration: 0.5, ease: "bounce.inOut" } });
-            tl.to(logo.current, {
-                  opacity: 0.1,
-                  filter: "blur(10px)",
-            })
-                  .to(img.current, { x: 100 }, "-=0.5")
-                  .to(img.current, { scale: 3.2 }, "-=0.5");
-      }, []);
       return (
             <section className="flex justify-center  items-center w-screen h-screen fixed z-50 top-0 left-0 bg-[var(--color-wht)]/30 backdrop-blur-lg text-[var(--color-bl)] ">
                   <div
@@ -23,11 +8,9 @@ const ScreenLoader = () => {
                         }}
                   />
                   <div className="w-1/ absolute h-1/2 flex gap-5 justify-center items-center">
-                        <img ref={img} src="/logo.svg" alt="icon" />
+                        <img src="/logo.svg" alt="icon" />
                         <div className="overflow-hidden">
-                              <h1 ref={logo} className="font-feeling-logo text-5xl">
-                                    Minima
-                              </h1>
+                              <h1 className="font-feeling-logo text-5xl">Minima</h1>
                         </div>
                   </div>
             </section>
